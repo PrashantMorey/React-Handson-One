35481ec52dd53c4351374c041851d634ebcccd9c
Prashant Morey <prashantmorey88308@gmail.com>
2023-04-28 20:02:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component } from "react";
+import './App.css';
+import MyFunctionalComponent from './Components/MyFunctunalCpmponent';
+import MyClassComponent from "./Components/MyClassComponent";
+import "./Components/myStyle.css";
+
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showFunctionalComponent: true,
+      showClassComponent: true,
+    };
+  }
+
+  toggleFunComp = () => {
+    this.setState((prevState) => ({
+      showFunctionalComponent : !prevState.showFunctionalComponent,
+    }));
+  }
+  toggleClassComp = () => {
+    this.setState((prevState) => ({
+      showClassComponent : !prevState.showClassComponent,
+    }));
+  }
+
+
+  render(){
+  return ( 
+  <div className="App">
+    <h1 className="grt">Styling using Functional and Class Component</h1> 
+    <div className="twoBox">
+    <button onClick={this.toggleFunComp} className="firstBox">To see styling functional component</button>
+    <button onClick={this.toggleClassComp} className="secondBox">To see styling in class component</button>
     </div>
-  );
+    <div className="twoBigBoxes">
+    {this.state.showFunctionalComponent && <MyFunctionalComponent/>}
+    {this.state.showClassComponent && <MyClassComponent/>}
+    </div>
+  </div>
+  )
+}
 }
 
 export default App;
---
+++ src/Components/MyFunctunalCpmponent.js
@@ -0,0 +1,15 @@
+import "./myStyle.css";
+
+function MyFunctionalComponent(){
+    return        <div className="firstBigBox">
+            <h1>This is created using functional Component</h1>
+            <p>This is done using external CSS</p>
+            <p style={{color:"blue"}}>This is done using inline CSS</p>
+        </div>
+
+
+
+    
+}
+
+export default MyFunctionalComponent
